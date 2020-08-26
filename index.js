0dfe71d57bdc50f20c10b804cc8abc64d966ca6b
Ascaliko <ascaliko7@gmail.com>
2020-08-26 02:25:53 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var animal = 'dog';
+ 
+function makeZoo() {
+  var animal = 'cat';
+ 
+  console.log(`I think I'll put this ${animal} in the zoo.`);
+}
+ 
+makeZoo(); // "I think I'll put this cat in the zoo."
+ 
+animal // "dog"
